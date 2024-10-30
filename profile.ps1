919a2ebedff66342866834bae02a412678647ff2
valerio-pescatori <pescatorim14@gmail.com>
2024-10-30 21:23:29 +0100

    feat: update config
    adds alias-grep function
    adds theme
    adds oh-my-posh init
    adds zoxide

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,11 @@
-oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/catppuccin_mocha.omp.json | Invoke-Expression
-Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/catppuccin_macchiato.omp.json' | Invoke-Expression
+Invoke-Expression (& { (zoxide init --cmd cd  powershell | Out-String) })
+Import-Module Catppuccin
 Import-Module git-aliases -DisableNameChecking
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineKeyHandler -Key "Tab" -Function AcceptSuggestion
+Set-PSReadLineKeyHandler -Key Tab -Function AcceptSuggestion
+
+function alias-grep {
+  param ($SearchString)
+  Get-Command -Module git-aliases | Where-Object {$_.Definition -like "*$SearchString*"}
+}
